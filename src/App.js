2ec2b8006f4665c20200821df4b09427b395d137
trentng99 <trentngqx@gmail.com>
2022-07-18 22:54:51 +0800

    Setting up the pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import ThemeSwitcher from './components/ThemeSwitches';
+import ThemeSwitcher from './pages/ThemeSwitches';
 import { AuthProvider } from './contexts/AuthContext';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import LandingScreen from './pages/LandingScreen';
 
 function App() {
   return (
     <Router>
     <AuthProvider>
       <Routes>
-        <Route path="/" element={<ThemeSwitcher />} />
+        <Route path="/" element={<LandingScreen />} />
+        <Route path="/switches" element={<ThemeSwitcher />} />
         {/* <Route element={<PrivateRoute user={user} redirectPath="/login" />}>
           <Route path="dashboard" element = {<Dashboard />} />
         </Route> */}
